dd892648c0d46477dfc70f45240e8b8f363d535b
Anshu Rai <anshu.rai018@gmail.com>
2024-02-29 08:25:32 +0530

    Delete Aura Components Specialist directory

--- Aura Components Specialist/README.md
+++
@@ -1,6 +0,0 @@
-# What this repository contains:
-**All code for the Aura Components Specialist superbadge, including:**
-1. Aura component bundles
-2. Apex controllers for remote actions
-3. Apex triggers
-4. Apex test classes
--- Aura Components Specialist/Superbadge Code/aura/BoatSearch/BoatSearchController.js
+++
@@ -1,7 +0,0 @@
-({
-	onFormSubmit : function(component, event, helper) {
-		var formSubmitData = event.getParam("formData").boatTypeId;
-        var childCmp = component.find("boatsearchresults");
-        childCmp.search(formSubmitData);
-	}
-})
